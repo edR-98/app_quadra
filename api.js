ca8ce1b5410e4c8d357f2def2a1a47d13b0e685d
GabesArcal <gabesarcal215@gmail.com>
2024-10-09 21:21:02 -0300

    Adicionando ./routes/"nome" no api.js

--- api.js
+++ api.js
@@ -3,14 +3,14 @@ const express = require('express')
 const app = express()
 app.use( express .json ())
 
-const cr_user = require('./user')
+const cr_user = require('./routes/user')
 app.use('/user', cr_user)
 
-const cr_quadra = require('./quadra')
-app.use('/quadra', cr_quadra)
+//const cr_quadra = require('./routes/quadra')
+//app.use('/quadra', cr_quadra)
 
-const cr_evento = require('./evento')
-app.use('/evento', cr_evento)
+//const cr_evento = require('./routes/evento')
+//app.use('/evento', cr_evento)
 
 const cr_pagamento = require('./routes/pagamento')
 app.use('/pagamento', cr_pagamento)
