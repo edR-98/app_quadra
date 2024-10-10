cb5e06091a3800a68e1eb9df764c82d7e275818d
Eduardo <eduardo.mcd@hotmail.com>
2024-10-09 21:36:47 -0300

    Correções mínimas

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ router.get( '/show/:id', cuser.get_user_by_id)
 router.put( '/update/:id', cuser.put_user_by_id)
 
 
-router.delete('delete/:id', cuser.delete_user)
+router.delete('/delete/:id', cuser.delete_user)
             
 module.exports = router
 
